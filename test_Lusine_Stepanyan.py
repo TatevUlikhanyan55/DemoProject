c076bfa1f5949550e5cf1b9aeb33b4c41e51ad39
LusinStep <lucy.stepanyan28@gmail.com>
2023-05-27 23:54:07 +0400

    Add also surname and age properties and print them

--- test_Lusine_Stepanyan.py
+++ test_Lusine_Stepanyan.py
@@ -1,2 +1,2 @@
-def test_print_football_player_name(name="Leo Messi"):
-    print(f"His name is {name}")
+def test_print_football_player_name(name="Leo",surname="Messi", age=35):
+    print(f"Football player name is {name}, surname is {surname} and he is {age} old.")

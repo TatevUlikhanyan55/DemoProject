9834beec6eb7c9f60a67aa4dc07234b23c2184d9
arkadisafaryan <888safaryan@hmail.com>
2023-05-28 14:44:59 +0400

    Added fav car

--- test_Arkadi_Safaryan.py
+++ test_Arkadi_Safaryan.py
@@ -1,2 +1,2 @@
-def test_print_name (name = 'Arkadi', surname = 'Safaryan'):
-    print(f'My name is {name}, surname is {surname}')
+def test_print_name (name = 'Arkadi', surname = 'Safaryan', car = 'Mercedes-Benz'):
+    print(f'My name is {name}, surname is {surname}. \nMy fav car is {car}.')

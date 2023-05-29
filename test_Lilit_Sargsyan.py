b0bd3c456ea7ea50886ed5e69b3c35290a0e066b
lilitsar <lilitsargsyan4@gmail.com>
2023-05-29 23:20:09 +0400

    added new file

--- test_Lilit_Sargsyan.py
+++ test_Lilit_Sargsyan.py
@@ -1,2 +1,4 @@
 def test_print_name(name="Lilit",surname="Sargsyan",nickname="sarly"):
-    print(f"My name is {name} {surname}, and my nickname is {nickname}")
+    print(f"My name is {name} {surname}, and my nickname is {nickname},"
+          f"it's a short version of my {name} and {surname} together,"
+          f"My name is the name of the goddess of the dark side of the Moon :) ")

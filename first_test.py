44db65a7ebaf0ea2f66c4a2568d381572158b3bd
arman1safaryan <arman1safaryan@gmail.com>
2023-05-29 19:01:33 +0400

    empty lines removed

--- first_test.py
+++ first_test.py
@@ -1,27 +1,15 @@
 def print_function():
     print("Best Wishes")
-
     print("gjsg")
     print("hhhhh")
     print("gjshhhg")
-
-=======
-
-=======
     print("test GG")
     print("Grigor Grigoryan")
     print("Grigor GG")
-      print("gjsg")
+    print("gjsg")
     print("hjsgdhgfhs")
-
-
-
-=======
     print("gjsg")
     print("Chnaged by lilitsar")
     print("Areg12")
-    
-    
 print("1111")
 print("123")
-
